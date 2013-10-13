569cb16111f43ea23def9a66b347c5993fef6897
Ilnaz Fazliakhmetov <ilnaz94@gmail.com>
2013-10-13 12:55:10 +0400

    improving enum

--- Task2/src/Card.java
+++ Task2/src/Card.java
@@ -1,11 +1,9 @@
 
 public class Card {
 	public Suits suit;
-	public String rang;
+	public Rangs rang;
 	
-	final static String[] rangs = {"A","6","7","8","9","10","J","Q","K"};
-	
-	Card(Suits suit, String rang) {
+	Card(Suits suit, Rangs rang) {
 		this.suit = suit;
 		this.rang = rang;
 	}
