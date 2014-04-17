44d7ad4c08e77a6cbce9ef63efc3ba8a5ad4db27
Ilnaz Fazliakhmetov <ilnaz94@gmail.com>
2014-04-17 22:12:43 +0400

    added Task16

---
+++ term4/Task16/src/main/java/model/Category.java
@@ -0,0 +1,62 @@
+package model;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(generator="increment")
+    @GenericGenerator(name="increment", strategy = "increment")
+    private Long id;
+    private String name;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<CV> cvs;
+
+    @OneToMany(mappedBy = "category")
+    private Set<Vacancy> vacancies;
+
+
+    public Category() {
+    }
+
+    public Category(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<CV> getCvs() {
+        return cvs;
+    }
+
+    public void setCvs(Set<CV> cvs) {
+        this.cvs = cvs;
+    }
+
+    public Set<Vacancy> getVacancies() {
+        return vacancies;
+    }
+
+    public void setVacancies(Set<Vacancy> vacancies) {
+        this.vacancies = vacancies;
+    }
+}
